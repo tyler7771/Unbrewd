80c4c2c1012bfd0355fb156b35bc0a958ec6512c
Tyler Fields <TylerFields@Tylers-MacBook-Pro.local>
2016-11-03 11:28:49 -0700

    fix errors, style errors and nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
+  validates :username, uniqueness: true
 
   attr_reader :password
   after_initialize :ensure_session_token
 
   def self.find_by_credintials(username, password)
     user = User.find_by(username: username)
-    return nil unless user && user.is_password?(password)
+    return nil unless user
+    return "password" unless user.is_password?(password)
     user
   end
 
